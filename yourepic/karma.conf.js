57c494d4ad793f82f1abaf3adebec493aa577ad8
Antonio Mendez <mendac01@gmail.com>
2020-12-17 23:25:10 -0500

    added chrome support

--- yourepic/karma.conf.js
+++ yourepic/karma.conf.js
@@ -1,5 +1,7 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
+const process = require('process');
+process.env.CHROME_BIN = require('puppeteer').executablePath();
 
 module.exports = function (config) {
   config.set({
@@ -32,6 +34,7 @@ module.exports = function (config) {
         { type: 'text-summary' }
       ]
     },
+    browsers: ['ChromeHeadless'],
     reporters: ['progress', 'kjhtml'],
     port: 9876,
     colors: true,
