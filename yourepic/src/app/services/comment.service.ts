2b673d925baa8c4a2973213b9bbc4de5562fb6b9
Antonio Mendez <mendac01@gmail.com>
2020-12-20 19:03:10 -0500

    finished comment service

--- yourepic/src/app/services/comment.service.ts
+++ yourepic/src/app/services/comment.service.ts
@@ -1,9 +1,30 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment'
+import Comment from '../interfaces/category';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CommentService {
+  private baseUrl = environment.baseUrl;
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getCommentsForEpicId(epicId: number): Observable<Comment[]> {
+    return this.http.get<Comment[]>(`${this.baseUrl}/comments/${epicId}`)
+  }
+
+  postNewCommentForEpicId(comment: Comment) {
+    return this.http.post(`${this.baseUrl}/comments`, comment)
+  }
+
+  replyToComment(epicId: number, comment: Comment) {
+    return this.http.post(`${this.baseUrl}/comments/comment/${epicId}`, comment)
+  }
+
+  deleteComment(commentId:number){
+    return this.http.delete(`$${this.baseUrl}/comments/${commentId}`)
+  }
 }
