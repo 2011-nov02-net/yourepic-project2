5616e764cb6a25378fb422a6ccb6f735b0026dc8
dannydjg16 <dannydjg16@gmail.com>
2020-12-20 20:14:13 -0500

    Login works, but logout had CORS restrictions

--- yourepic/src/app/app.component.ts
+++ yourepic/src/app/app.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { OktaAuthService } from '@okta/okta-angular'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit {
+  isAuthenticated = false;
+
+  constructor(private oktaAuth: OktaAuthService) {
+    this.oktaAuth.$authenticationState.subscribe((isAuthenticated) => 
+    this.updateAuthState(isAuthenticated));
+  }
+
+  ngOnInit(): void {
+    debugger
+    this.oktaAuth.isAuthenticated().then((isAuthenticated) => this.updateAuthState(isAuthenticated));
+  }
+  
+  updateAuthState(isAuthenticated: boolean) {
+    debugger
+    this.isAuthenticated = isAuthenticated;
+    if (isAuthenticated) {
+      this.oktaAuth.getUser().then(console.log);
+    }
+  }
+
+  login() {
+    this.oktaAuth.signInWithRedirect();
+  }
+
+  logout() {
+    debugger
+    this.oktaAuth.signOut();
+  }
   title = 'yourepic';
 }
--- yourepic/src/app/app.module.ts
+++ yourepic/src/app/app.module.ts
@@ -3,6 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { OktaAuthModule, OktaCallbackComponent, OKTA_CONFIG } from '@okta/okta-angular';
+
+const config = {
+  issuer: 'https://dev-7824301.okta.com/oauth2/default',
+  pkce: true,
+  clientId: '0oa2s0wrcsc5h3BYT5d6',
+  redirectUri: `${window.location.origin}/login/callback`,
+  scopes: ['openid'],
+  postLogoutRedirectUri: window.location.origin,
+};
 
 @NgModule({
   declarations: [
@@ -10,9 +20,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    AppRoutingModule,
+    OktaAuthModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [{ provide: OKTA_CONFIG, useValue: config}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
