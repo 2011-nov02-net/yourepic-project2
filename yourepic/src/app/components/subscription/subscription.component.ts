656c7c3db817dc2be7c63297ca655151c45b1867
Alex Soto <alex.soto0624@gmail.com>
2020-12-21 22:54:21 -0800

    Added subscriptions componet

---
+++ yourepic/src/app/components/subscription/subscription.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import Subscription from 'src/app/interfaces/subscription';
+import { SubscriptionService } from 'src/app/services/subscription.service';
+import { UserService } from 'src/app/services/user.service'
+
+@Component({
+  selector: 'app-subscription',
+  templateUrl: './subscription.component.html',
+  styleUrls: ['./subscription.component.css']
+})
+export class SubscriptionComponent implements OnInit {
+
+  subscriptions : Subscription[] | null = null;
+
+  constructor(private subscriptionService : SubscriptionService) {}
+
+  ngOnInit(): void {
+    this.subscriptionService.getSubscriptionsForUser(2)
+    .toPromise()
+    .then(items => {
+      this.subscriptions = items
+    })
+    
+  }
+
+}
