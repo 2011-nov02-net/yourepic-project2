1777dc707f10ea28056bb4513e1818542d581b6b
dannydjg16 <dannydjg16@gmail.com>
2020-12-22 15:39:24 -0500

    now you can get chapters based on the epic that was clicked. want to make a screen to present the chapter and have it change based on what the selected chapter is

--- yourepic/src/app/components/chapters-sidebar/chapters-sidebar.component.ts
+++ yourepic/src/app/components/chapters-sidebar/chapters-sidebar.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from '@angular/core';
 import Chapter from 'src/app/interfaces/chapter';
 import EpicService  from 'src/app/services/epic.service';
 import { UserService } from 'src/app/services/user.service';
+import { ActivatedRoute } from '@angular/router';
  
 @Component({
  selector: 'app-chapters-sidebar',
@@ -11,16 +12,21 @@ import { UserService } from 'src/app/services/user.service';
 })
 export class ChaptersSidebarComponent implements OnInit {
  
- chapters: Chapter[] | null = null;
- viewChapter!: Chapter;
- constructor(private epicService: EpicService) { }
+  chapters: Chapter[] | null = null;
+  viewChapter!: Chapter;
+  constructor(private epicService: EpicService, private route: ActivatedRoute) { }
  
- ngOnInit(): void {
-   this.epicService.getChaptersForEpic(2)
-   .then(items => { this.chapters = items; this.viewChapter = items[0]});
- }
+  ngOnInit(): void {
+   this.getChapters();
+  }
+
+  getChapters(): void {
+    const id: number = this.route.snapshot.paramMap.get('id')! as unknown as number;
+    this.epicService.getChaptersForEpic(id)
+        .then(items => { this.chapters = items; this.viewChapter = items[0]});
+    }
  
- onSelect(chapter: Chapter): void {
-   this.viewChapter = chapter;
+  onSelect(chapter: Chapter): void {
+    this.viewChapter = chapter;
  }
 }
