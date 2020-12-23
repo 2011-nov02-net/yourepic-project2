f55da9156e5d6a00d9cec6e8f40009ee73a73c29
dannydjg16 <dannydjg16@gmail.com>
2020-12-22 20:27:14 -0500

    pre merge

---
+++ yourepic/src/app/components/create-epic/create-epic.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-create-epic',
+  templateUrl: './create-epic.component.html',
+  styleUrls: ['./create-epic.component.css']
+})
+export class CreateEpicComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
