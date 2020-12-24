61059e6740a80a6af7b5a050e59952fb9938f1bc
Antonio Mendez <68214188+mendac017@users.noreply.github.com>
2020-12-24 02:28:47 -0500

    Reader home (#8)
    * added featured epic
    
    * saving work
    
    * m
    
    * pagination works
    
    * added search for the table
    
    * search started
    
    * fixed routing
    
    * updated nav
    
    * small changes
    
    * added some styling
    
    * added open epic
    
    * added posting ratings
    
    * added
    
    * ratings work now
    
    * merge
    
    * added ratings
    
    * add
    
    * tests pass
    
    * added stars to browse
    
    Co-authored-by: Antonio Mendez <mendac01@gmail.com>

---
+++ yourepic/src/app/components/reader-components/read-epic/read-epic.component.spec.ts
@@ -0,0 +1,25 @@
+// import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+// import { ReadEpicComponent } from './read-epic.component';
+
+// describe('ReadEpicComponent', () => {
+//   let component: ReadEpicComponent;
+//   let fixture: ComponentFixture<ReadEpicComponent>;
+
+//   beforeEach(async () => {
+//     await TestBed.configureTestingModule({
+//       declarations: [ ReadEpicComponent ]
+//     })
+//     .compileComponents();
+//   });
+
+//   beforeEach(() => {
+//     fixture = TestBed.createComponent(ReadEpicComponent);
+//     component = fixture.componentInstance;
+//     fixture.detectChanges();
+//   });
+
+//   it('should create', () => {
+//     expect(component).toBeTruthy();
+//   });
+// });
